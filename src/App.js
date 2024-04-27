b961c6512798a28f9db03c7fe3c806b38521dbef
Fujitaka Okarashii <nuttli.christian@gmail.com>
2024-04-27 15:24:56 +0200

    - refactored TransferList to SelectableList

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
-import TransferList from './components/TransferList/TransferList';
+import SelectableList from './components/TransferList/SelectableList';
+import Grid from "@mui/material/Grid";
+import List from "@mui/material/List";
+import Button from "@mui/material/Button";
+import { useState } from 'react';
+
+function not(a, b) {
+	return a.filter((value) => b.indexOf(value) === -1);
+}
+
+function intersection(a, b) {
+	return a.filter((value) => b.indexOf(value) !== -1);
+}
 
 export default function App() {
 	const people = [
@@ -11,7 +23,99 @@ export default function App() {
 		"Pastor",
 		"Teacher",
 	];
-	return(
-		<TransferList list={people}/>
+
+	const [checked, setChecked] = useState([]);
+	const [left, setLeft] = useState(people);
+	const [right, setRight] = useState([]);
+
+	const leftChecked = intersection(checked, left);
+	const rightChecked = intersection(checked, right);
+
+	const handleToggle = (value) => () => {
+		const currentIndex = checked.indexOf(value);
+		const newChecked = [...checked];
+
+		if (currentIndex === -1) {
+			newChecked.push(value);
+		} else {
+			newChecked.splice(currentIndex, 1);
+		}
+
+		setChecked(newChecked);
+	};
+
+	const handleAllRight = () => {
+		setRight(right.concat(left));
+		setLeft([]);
+	};
+
+	const handleCheckedRight = () => {
+		setRight(right.concat(leftChecked));
+		setLeft(not(left, leftChecked));
+		setChecked(not(checked, leftChecked));
+	};
+
+	const handleCheckedLeft = () => {
+		setLeft(left.concat(rightChecked));
+		setRight(not(right, rightChecked));
+		setChecked(not(checked, rightChecked));
+	};
+
+	const handleAllLeft = () => {
+		setLeft(left.concat(right));
+		setRight([]);
+	};
+
+	return (
+		<Grid container spacing={2} justifyContent="center" alignItems="center">
+			<Grid item>
+				<SelectableList list={left} header="Candidates" handleToggle={handleToggle} checked={checked}/>
+			</Grid>
+
+			<Grid item>
+				<Grid container direction="column" alignItems="center">
+					<Button
+						sx={{ my: 0.5 }}
+						variant="outlined"
+						size="small"
+						onClick={handleAllRight}
+						disabled={left.length === 0}
+						aria-label="move all right">
+						≫
+					</Button>
+					<Button
+						sx={{ my: 0.5 }}
+						variant="outlined"
+						size="small"
+						onClick={handleCheckedRight}
+						disabled={leftChecked.length === 0}
+						aria-label="move selected right">
+						&gt;
+					</Button>
+					<Button
+						sx={{ my: 0.5 }}
+						variant="outlined"
+						size="small"
+						onClick={handleCheckedLeft}
+						disabled={rightChecked.length === 0}
+						aria-label="move selected left">
+						&lt;
+					</Button>
+					<Button
+						sx={{ my: 0.5 }}
+						variant="outlined"
+						size="small"
+						onClick={handleAllLeft}
+						disabled={right.length === 0}
+						aria-label="move all left">
+						≪
+					</Button>
+				</Grid>
+			</Grid>
+
+			<Grid item>
+				<SelectableList list={right} header="Heroes" handleToggle={handleToggle} checked={checked}/>
+			</Grid>
+		</Grid>
 	)
 }
---
+++ src/components/TransferList/SelectableList.jsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+import Grid from "@mui/material/Grid";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Checkbox from "@mui/material/Checkbox";
+import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
+import Typography from '@mui/material/Typography';
+import { Card, CardHeader, Divider } from "@mui/material";
+
+export default function SelectableList({ list, header, handleToggle, checked }) {
+	return (
+		<Card>
+			<CardHeader sx={{ px: 2, py: 1}} title={header} titleTypographyProps={{align: "center", paddingY: "0.5rem"}}/>
+			<Divider/>
+			<List dense component="div" role="list" sx={{ width: 200, height: 420, overflow: "auto", bgcolor: "background.paper"}}>
+				{list.map((value) => {
+					const labelId = `transfer-list-item-${value}-label`;
+
+					return (
+						<ListItemButton
+							key={value}
+							role="listitem"
+							onClick={handleToggle(value)}>
+							<ListItemIcon>
+								<Checkbox
+									checked={checked.indexOf(value) !== -1}
+									tabIndex={-1}
+									disableRipple
+									inputProps={{
+										"aria-labelledby": labelId,
+									}}
+								/>
+							</ListItemIcon>
+							<ListItemText id={labelId} primary={value} />
+						</ListItemButton>
+					);
+				})}
+			</List>
+		</Card>
+	);
+}
